ae507d58e6eae277f86402bf0b83e402cd427334
ltbur <ltburtonst@gmail.com>
2022-06-23 10:58:00 +0900

    ファイル追加

---
+++ js/main.js
@@ -0,0 +1,71 @@
+'use strict';
+document.addEventListener("DOMContentLoaded",()=>{
+  const size=4;//盤面の大きさ
+  const difficulty=500;//シャッフルの複雑さ
+  const shuffleCount=size*difficulty;
+  const table=document.getElementById("table");
+  const msgBox=document.getElementById("msgBox");
+  const startBt=document.getElementById("startBt");
+  let panels;//td要素を格納する配列
+  let emptyIdx;//現在の空のindexを保持
+
+  const init = ()=>{
+    panels=[];
+    table.textContent=null;
+    msgBox.textContent=null;
+    createStage();
+  };
+  const createStage = ()=>{
+    for(let i=0;i<size;i++){
+      const tr=document.createElement("tr");
+      for(let j=0;j<size;j++){
+        const td=document.createElement("td");
+        td.posId=i*size+j;
+        td.textContent=td.posId+1;
+        if(td.posId === size*size-1){
+          td.textContent="";
+          td.classList.add("empty");
+          emptyIdx=td.posId;
+        }
+        tr.append(td);
+        panels.push(td);
+      }
+      table.append(tr);
+    }
+  };
+  startBt.addEventListener("click",()=>{
+    for(let i=0;i<shuffleCount;i++){
+      const dir=Math.floor(Math.random()*4);//0上,1右,2下,3左
+      switch(dir){
+        case 0:
+          if(emptyIdx < size) continue;
+          swap(emptyIdx-size,emptyIdx);
+          emptyIdx-=size;
+          break;
+        case 1:
+          if((emptyIdx+1)% size === 0) continue;
+          swap(emptyIdx+1,emptyIdx);
+          emptyIdx++;
+          break;
+        case 2:
+          if(emptyIdx >= size*(size-1)) continue;
+          swap(emptyIdx+size,emptyIdx);
+          emptyIdx+=size;
+          break;
+        case 3:
+          if(emptyIdx % size === 0) continue;
+          swap(emptyIdx-1,emptyIdx);
+          emptyIdx--;
+          break;
+      }
+    }
+
+  });
+  const swap=(numPos,empPos)=>{
+    panels[empPos].textContent=panels[numPos].textContent;
+    panels[empPos].classList.remove("empty");
+    panels[numPos].textContent="";
+    panels[numPos].classList.add("empty");
+  };
+  init();
+});
